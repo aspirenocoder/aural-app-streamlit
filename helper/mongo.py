ac5705171468b4708b76901f785694cdb9f0dd76
apple <apple@apples-MacBook-Pro.local>
2025-04-01 15:16:38 +0530

    first commit

---
+++ helper/mongo.py
@@ -0,0 +1,59 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+# Get the current timestamp
+current_timestamp = datetime.now()
+
+# Format the timestamp
+formatted_timestamp = current_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+# Connect to MongoDB
+client = MongoClient(
+    "mongodb+srv://srinivasanethiraaj:1ZTaq7Atz4qVjfkW@generations.unxcj.mongodb.net/"
+)
+db = client["test"]
+collection = db["entries"]
+
+Dcollection = db["dentries"]
+
+# current_time = int(time.time())
+
+
+# Insert documents
+def insert(text, airesp, ipt, opt, tt):
+
+    try:
+        collection.insert_one(
+            {
+                "user_id": "Vaasa",
+                "user_text": text,
+                "AI_response": airesp,
+                "input_tokens": ipt,
+                "output_tokens": opt,
+                "total_tokens": tt,
+                "created_at": formatted_timestamp,
+                "updated_at": formatted_timestamp,
+            }
+        )
+        # client.close()
+    except Exception as e:
+        raise RuntimeError(f"Error inserting into MongoDB: {e}")
+
+
+def dinsert(text, airesp, ipt, opt, tt):
+    try:
+        Dcollection.insert_one(
+            {
+                "user_id": "Vaasa",
+                "Duser_text": text,
+                "AI_response": airesp,
+                "input_tokens": ipt,
+                "output_tokens": opt,
+                "total_tokens": tt,
+                "created_at": formatted_timestamp,
+                "updated_at": formatted_timestamp,
+            }
+        )
+        # client.close()
+    except Exception as e:
+        raise RuntimeError(f"Error inserting into MongoDB: {e}")
